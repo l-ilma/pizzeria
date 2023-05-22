dc5e29d54016d2bf08b906b0079ea3f280f25919
Haris Hadzimahmutovic <haris.hadzimahmutovic@student.tugraz.at>
2023-05-22 20:44:11 +0200

    add pizza list fragment

--- Android/app/src/main/java/com/example/pizzeria/PizzaArrayAdapter.java
+++ Android/app/src/main/java/com/example/pizzeria/PizzaArrayAdapter.java
@@ -42,24 +42,31 @@ public class PizzaArrayAdapter extends BaseAdapter {
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+
         LinearLayout listEntry = new LinearLayout(mContext);
         listEntry.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
         listEntry.setOrientation(LinearLayout.HORIZONTAL);
 
         ImageView pizzaImage = new ImageView(mContext);
-        pizzaImage.setLayoutParams(new FrameLayout.LayoutParams(200, 200));
+        pizzaImage.setLayoutParams(new FrameLayout.LayoutParams((int)(125 * scale * 0.5f), (int)(125 * scale * 0.5f)));
         pizzaImage.setId(position);
         pizzaImage.setImageResource(mThumbIds.get(position));
         pizzaImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
+        TextView firstEmptyTextView = new TextView(mContext);
+        LinearLayout.LayoutParams firstEmptyTextParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+        firstEmptyTextParams.weight = 1;
+        firstEmptyTextView.setLayoutParams(firstEmptyTextParams);
+
         TextView pizzaName = new TextView(mContext);
         LinearLayout.LayoutParams nameParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        nameParams.topMargin = 40;
-        nameParams.leftMargin = 40;
+        nameParams.topMargin = (int)(45 * scale * 0.5f);
+        nameParams.leftMargin = (int)(40 * scale * 0.5f);
         pizzaName.setLayoutParams(nameParams);
         pizzaName.setGravity(Gravity.RIGHT);
         pizzaName.setText("Some pizza name");
-        pizzaName.setTextColor(mContext.getResources().getColor(R.color.white));
+        pizzaName.setTextColor(mContext.getResources().getColor(R.color.black));
 
         TextView emptyTextView = new TextView(mContext);
         LinearLayout.LayoutParams emptyTextParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -68,17 +75,26 @@ public class PizzaArrayAdapter extends BaseAdapter {
 
         TextView pizzaPrice = new TextView(mContext);
         LinearLayout.LayoutParams priceParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        priceParams.topMargin = 40;
-        priceParams.leftMargin = 40;
+        priceParams.topMargin = (int)(45 * scale * 0.5f);
+        priceParams.leftMargin = (int)(40 * scale * 0.5f);
         pizzaPrice.setLayoutParams(priceParams);
         pizzaPrice.setGravity(Gravity.RIGHT);
         pizzaPrice.setText("20€");
-        pizzaPrice.setTextColor(mContext.getResources().getColor(R.color.white));
+        pizzaPrice.setTextColor(mContext.getResources().getColor(R.color.black));
+
+        ImageView cartButton = new ImageView(mContext);
+        LinearLayout.LayoutParams cartButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        cartButtonParams.rightMargin = (int) (20 * scale * 0.5f);
+        cartButtonParams.topMargin = (int) (40 * scale * 0.5f);
+        cartButton.setLayoutParams(cartButtonParams);
+        cartButton.setImageResource(R.drawable.ic_add_to_cart);
 
         listEntry.addView(pizzaImage);
         listEntry.addView(pizzaName);
-        listEntry.addView(emptyTextView);
+        listEntry.addView(firstEmptyTextView);
         listEntry.addView(pizzaPrice);
+        listEntry.addView(emptyTextView);
+        listEntry.addView(cartButton);
 
         return listEntry;
     }
--- Android/app/src/main/java/com/example/pizzeria/menu/PizzaMenuFragment.java
+++ Android/app/src/main/java/com/example/pizzeria/menu/PizzaMenuFragment.java
@@ -1,7 +1,55 @@
 package com.example.pizzeria.menu;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.pizzeria.PizzaArrayAdapter;
+import com.example.pizzeria.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class PizzaMenuFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View pizzaMenuView = inflater.inflate(R.layout.pizza_menu_fragment, container, false);
+        ListView pizzaList = pizzaMenuView.findViewById(R.id.pizzaList);
+
+        List<Integer> pizzas = new ArrayList<>();
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+        pizzas.add(R.drawable.ic_pizza_mozzarela);
+
+        pizzaList.setAdapter(new PizzaArrayAdapter(inflater.getContext(), pizzas));
+        return pizzaMenuView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
